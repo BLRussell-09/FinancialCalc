339718bbb8105e8a04f1d5bf35a95f2ad2094222
Barry Russell <brussell@FORTERACU.COM>
2019-05-20 11:26:13 -0500

    renamed id's changed stylesheet to materialize

--- javascripts/clickEvent.js
+++ javascripts/clickEvent.js
@@ -7,15 +7,12 @@ function buttonClick()
 function netTotal()
 {
   var desiredNet = document.getElementById('desNet').value * 1;
-  var withholdings = document.getElementById('totalWith').value * 1;
-  var stateWithholdings = document.getElementById('fee').value * 1;
-  var totalWithholdings = (withholdings + stateWithholdings);
-  //var fee = document.getElementById('fee').value * 1;
+  var federalWithholdings = document.getElementById('federalWith').value * 1;
+  var stateWithholdings = document.getElementById('stateWith').value * 1;
+  var totalWithholdings = (federalWithholdings + stateWithholdings);
   var total = document.getElementById('total');
-  console.log(totalWithholdings);
   totalWithholdings = 1 - (totalWithholdings / 100);
   var sum = desiredNet / totalWithholdings;
-  console.log(totalWithholdings);
   total.value = sum.toFixed(2)
 }
 
